574d1408a328686b350c5c5d9b2c8ec831532ceb
Kyle Westhaus <kwesthaus@users.noreply.github.com>
2020-08-09 13:54:46 -0400

    Change backup from weekly to nightly

--- usr/restic-backup/local/bin/restic-bak.sh
+++ usr/restic-backup/local/bin/restic-bak.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 restic -p /home/orville/.credentials/restic_home_NAS.txt -r sftp:NAS-restic-bak:/mnt/privdata/backups/restic-backups/home/ backup /home/orville/Configuration/ /home/orville/Desktop/ /home/orville/Documents/ --exclude-file=/home/orville/Documents/tech/documentation/mine/backup/restic-excludes.txt
-restic -p /home/orville/.credentials/restic_home_NAS.txt -r sftp:NAS-restic-bak:/mnt/privdata/backups/restic-backups/home/ forget --keep-weekly 5 --keep-monthly 12 --keep-yearly 100 --prune
+restic -p /home/orville/.credentials/restic_home_NAS.txt -r sftp:NAS-restic-bak:/mnt/privdata/backups/restic-backups/home/ forget --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 100 --prune
 restic -p /home/orville/.credentials/restic_home_NAS.txt -r sftp:NAS-restic-bak:/mnt/privdata/backups/restic-backups/home/ check
 
 source /home/orville/.credentials/env/backblaze
 
 restic -p /home/orville/.credentials/restic_home_offsite.txt -r b2:restic-backup-home backup /home/orville/Configuration/ /home/orville/Desktop/ /home/orville/Documents/ --exclude-file=/home/orville/Documents/tech/documentation/mine/backup/restic-excludes.txt
-restic -p /home/orville/.credentials/restic_home_offsite.txt -r b2:restic-backup-home forget --keep-weekly 5 --keep-monthly 12 --keep-yearly 100 --prune
+restic -p /home/orville/.credentials/restic_home_offsite.txt -r b2:restic-backup-home forget --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 100 --prune
 restic -p /home/orville/.credentials/restic_home_offsite.txt -r b2:restic-backup-home check
 
 source /home/orville/.credentials/env/backblaze-reset
