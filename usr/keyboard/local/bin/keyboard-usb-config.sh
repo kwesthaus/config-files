d7ba202086a1068b62475f75186041f703596c1d
Kyle Westhaus <kwesthaus@users.noreply.github.com>
2020-10-08 23:44:58 -0400

    Rework xkb shortcuts to avoid manual evdev copy
    Previously I created a copy of the evdev keycode file and manually
    chaged the keycodes, which I updated whenever the original evdev keycode
    file was changed by upstream. Additionally, users are supposed to modify
    keysyms, not keycodes. This change removes the custom xkb keycode file
    in favor of an xkb types file and an xkb symbols file. These new files
    have the additional advantage of extending the default configuration as
    opposed to completely replacing it. Finally, the Xkbmap dotfile has been
    updated to use the new xkb types and symbols. Though xkbcomp was
    recommended by many online, staying with setxkbmap allows the setup to
    continue working without modification of how lightdm starts the X
    session (as seen in /etc/lightdm/Xsession). Since options passed to the
    setxkbmap command completely replace those provided by the evdev rules,
    all types and symbols (not just the custom ones in the new files) are
    specified in the Xkbmap dotfile.

--- usr/keyboard/local/bin/keyboard-usb-config.sh
+++ usr/keyboard/local/bin/keyboard-usb-config.sh
@@ -3,4 +3,5 @@
 # If the sleep command is not included, we might set the configuration too fast before the system has a chance to set the default keyboard configuration to the device and our changes will get overwritten
 # According to some random guy on the internet, you have no way to determine when the system is done, so just waiting 1 second seems to work fine on my system
 # Additionally, running the command in the background is crucial because without it, the udev system cannot go ahead of us and set the default configuration while we wait so that we can overwrite it (instead of the udev system overwriting our configuration)
+# Note that the "setxkbmap `cat ...`" command below is the same as what occurs during startup of the X session by lightdm as seen in /etc/lightdm/Xsession
 /usr/bin/su orville -c "sleep 1; setxkbmap `cat /home/orville/.Xkbmap`" &
