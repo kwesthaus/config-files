69dda92f12ea82bbef740c08ae6074eb1289a9ed
Kyle Westhaus <kwesthaus@users.noreply.github.com>
2021-06-03 22:09:35 -0700

    mic-toggle: Include icon in notifications

--- usr/mic-toggle/local/bin/microphone-toggle.sh
+++ usr/mic-toggle/local/bin/microphone-toggle.sh
@@ -1,2 +1,3 @@
-amixer set Capture toggle && amixer get Capture | grep -q '\[off\]' && notify-send "MIC switched OFF" || notify-send "MIC switched ON"
+amixer set Capture toggle
+amixer get Capture | grep -q '\[off\]' && notify-send -i "microphone-sensitivity-muted-symbolic.symbolic" "MIC switched OFF" || notify-send -i "microphone-sensitivity-high-symbolic.symbolic" "MIC switched ON"
 amixer get Capture | grep -q '\[off\]' && sudo /usr/local/bin/microphone-led-off.sh || sudo /usr/local/bin/microphone-led-on.sh
