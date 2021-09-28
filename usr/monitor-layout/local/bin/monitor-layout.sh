0f691a34d4360e05c4f347c754172cbe4472f3af
Kyle Westhaus <kwesthaus@users.noreply.github.com>
2021-09-28 01:51:48 -0400

    xorg/i3: Reload background when monitors change

--- usr/monitor-layout/local/bin/monitor-layout.sh
+++ usr/monitor-layout/local/bin/monitor-layout.sh
@@ -7,21 +7,24 @@ hdmi_c=$?
 xrandr | rg "HDMI-1 disconnected"
 hdmi_d=$?
 
+# only run i3 commands if i3 is already started (NOT when running this script from ~/.xprofile)
+i3pid=$(pidof i3)
+
 if [ $builtin_c -eq 0 ] && [ $hdmi_c -eq 0 ]; then
     echo "both monitors attached"
     xrandr \
         --output eDP-1 --primary --mode 1920x1080 --pos 1600x0 --rotate normal \
         --output HDMI-1 --mode 1280x1024 --pos 0x0 --rotate normal --scale 1.25x1.25
+    [[ ! -z "$i3pid" ]] && i3-msg "restart"
 elif [ $builtin_c -eq 0 ] && [ $hdmi_d -eq 0 ]; then
     echo "only laptop monitor attached"
-    # only run i3 commands if i3 is already started (NOT when running this script from ~/.xprofile)
-    i3pid=$(pidof i3)
     # avoid moving to workspace that was focused on the other monitor
     [[ ! -z "$i3pid" ]] && focused=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused==true).num')
     xrandr \
         --output eDP-1 --primary --mode 1920x1080 --pos 1600x0 --rotate normal \
         --output HDMI-1 --off
     [[ ! -z "$i3pid" ]] && i3-msg "workspace --no-auto-back-and-forth number $focused"
+    [[ ! -z "$i3pid" ]] && i3-msg "restart"
 else
     echo "unexpected monitor state"
     return 255
