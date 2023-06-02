396d97280008b0ac579557e4d9deff38ee8998a7
Kyle Westhaus <kwesthaus@users.noreply.github.com>
2023-06-01 22:45:42 -0700

    battery-limit: Fix output text

--- usr/battery-limit/local/bin/update-charge-state.sh
+++ usr/battery-limit/local/bin/update-charge-state.sh
@@ -8,10 +8,10 @@ echo  "Percent: $CURRENT_PERCENT"
 echo  "Status: $CURRENT_STATUS"
 
 if [ "$CURRENT_STATUS" != "Discharging" ] && [ $CURRENT_PERCENT -ge 70 ]; then
-    echo "Charged above 70%, attempting to turn power on"
+    echo "Charged above 70%, attempting to turn power off"
     curl --silent --verbose "http://192.168.12.12/cm?user=admin&password=$HTTP_PASSWD&cmnd=Power%20off"
 elif [ "$CURRENT_STATUS" == "Discharging" ] && [ $CURRENT_PERCENT -le 65 ]; then
-    echo "Discharged below 65%, attempting to turn power off"
+    echo "Discharged below 65%, attempting to turn power on"
     curl --silent --verbose "http://192.168.12.12/cm?user=admin&password=$HTTP_PASSWD&cmnd=Power%20On"
 else
     echo "No power toggle needed, quitting"
